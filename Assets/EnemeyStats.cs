32b22331ce99266f2b89835674b5a139d993ae9f
achen052 <achen052@ucr.edu>
2014-04-28 17:09:57 -0700

    Updated Tower Mechanics
    - Added tower health
    - Added tower fire rate limit
    - Added tower resources and score
    - Added enemies giving resources and score on death.

---
+++ Assets/EnemeyStats.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+	public float mTheEnemyHealth = 100.0f;
+	public int mEnemyScore;
+	public int mEnemyResources;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(mTheEnemyHealth <= 0)
+		{
+			GameObject gc = GameObject.FindGameObjectWithTag("GameController");
+			if(gc)
+			{
+				SpawnWaves sw = gc.GetComponent<SpawnWaves>();
+				sw.numEnemiesRemaining--;
+			}
+			Destroy (gameObject);
+			
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Ammo")
+		{
+			Destroy (other.gameObject);
+			TurretAmmoBase getAmmoInfo = other.gameObject.GetComponent<TurretAmmoBase>();
+			float damageRate = getAmmoInfo.mTheDamage;
+			if(mTheEnemyHealth >= 0)
+				mTheEnemyHealth -= damageRate;
+		}
+
+	}
+}
