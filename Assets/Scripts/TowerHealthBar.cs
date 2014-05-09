e4d401dd06fb195928d8edba252071ecee312f90
Landon <lyang006@ucr.edu>
2014-05-08 20:33:34 -0700

    Samplehealthb
    sample

--- Assets/Scripts/TowerHealthBar.cs
+++ Assets/Scripts/TowerHealthBar.cs
@@ -5,10 +5,16 @@ public class TowerHealthBar : MonoBehaviour {
 
 	public int maxHealth = 20 ;
 	public int curHealth = 20 ;
-
-	//public Texture2D bgImage ;
-
+	public Texture2D bgImage ;
+	public Material mat ;
+	public float health = 100 ;
+	public float healthy ;
+	public float x = 0;
+	public float y = 0;
+	public float w ;
+	public float h ;
 	public float healthLength ;
+	public Rect box ;
 
 	public void ChangeHealth( int n )
 	{
@@ -18,24 +24,34 @@ public class TowerHealthBar : MonoBehaviour {
 		if (curHealth > maxHealth)
 			curHealth = maxHealth ;
 
-		healthLength = (Screen.width / 2) * (curHealth / (float)maxHealth);
+		healthLength = (272) * (curHealth / (float)maxHealth);
 
 	}
 	// Use this for initialization
 	void Start ()
 	{
-		healthLength = Screen.width / 2;
+		health = 100;
+		healthLength =300;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		healthy = 1 - (GameObject.Find("Main Camera").GetComponent<TowerHealthBar>().health/100) ;
+		if( healthy == 0 )
+			healthy = (float)0.1 ;
+		mat.SetFloat("_Cutoff", healthy) ;
 		ChangeHealth (0);
 	}
 
 	void OnGUI()
 	{
-		UnityEngine.GUI.Box (new Rect(10,10, healthLength, 20), curHealth + "/" + maxHealth);
+		if (Event.current.type.Equals (EventType.Repaint)) {
+			box = new Rect(x,y,w,h);
+			Graphics.DrawTexture(box, bgImage, mat );
+		}
+		UnityEngine.GUI.Box (new Rect(10,15, healthLength, 20), curHealth + "/" + maxHealth);
 		UnityEngine.GUI.Box (new Rect (550, 10, 150, 20), 
 		                     "Resources: " + GameObject.FindGameObjectWithTag("TheTower").GetComponent<TowerStats> ().mResources);
 	}
