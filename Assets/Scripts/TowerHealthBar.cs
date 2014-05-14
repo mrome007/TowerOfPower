371021bcd1c04fac6353b7df3128ded06c9cb41b
Landon <lyang006@ucr.edu>
2014-05-14 14:56:30 -0700

    HealthbarV1
    Still has some artifacts.

--- Assets/Scripts/TowerHealthBar.cs
+++ Assets/Scripts/TowerHealthBar.cs
@@ -5,16 +5,18 @@ public class TowerHealthBar : MonoBehaviour {
 
 	public int maxHealth = 20 ;
 	public int curHealth = 20 ;
-	public Texture2D bgImage ;
+	public Texture2D HBImage ;
+	public Texture2D HBbackground ;
 	public Material mat ;
 	public float health = 100 ;
-	public float healthy ;
+	public float healthy = 1;
 	public float x = 0;
 	public float y = 0;
-	public float w ;
-	public float h ;
+	public float w = 50;
+	public float h = Screen.width/3;
 	public float healthLength ;
 	public Rect box ;
+	public Rect boxback ;
 
 	public void ChangeHealth( int n )
 	{
@@ -32,7 +34,7 @@ public class TowerHealthBar : MonoBehaviour {
 	{
 		health = 100;
 		healthLength =300;
-
+		//mat.SetFloat("_Cutoff", (float)0.1) ;
 	}
 	
 	// Update is called once per frame
@@ -40,7 +42,7 @@ public class TowerHealthBar : MonoBehaviour {
 	{
 		healthy = 1 - (GameObject.Find("Main Camera").GetComponent<TowerHealthBar>().health/100) ;
 		if( healthy == 0 )
-			healthy = (float)0.1 ;
+			healthy = (float)0.04 ;
 		mat.SetFloat("_Cutoff", healthy) ;
 		ChangeHealth (0);
 	}
@@ -48,10 +50,12 @@ public class TowerHealthBar : MonoBehaviour {
 	void OnGUI()
 	{
 		if (Event.current.type.Equals (EventType.Repaint)) {
-			box = new Rect(x,y,w,h);
-			Graphics.DrawTexture(box, bgImage, mat );
+			boxback = new Rect(x,y*(float)1.15 +5,75*(float)1.15,Screen.height/2);
+			Graphics.DrawTexture(boxback, HBbackground );
+			box = new Rect(x+5,y,75,Screen.height/2);
+			Graphics.DrawTexture(box, HBImage, mat );
 		}
-		UnityEngine.GUI.Box (new Rect(10,15, healthLength, 20), curHealth + "/" + maxHealth);
+		UnityEngine.GUI.Box (new Rect(10,Screen.height/2 + 20, Screen.width/20, 20), curHealth + "/" + maxHealth);
 		UnityEngine.GUI.Box (new Rect (550, 10, 150, 20), 
 		                     "Resources: " + GameObject.FindGameObjectWithTag("TheTower").GetComponent<TowerStats> ().mResources);
 	}
