1389b1c4827d35109077c56908bedfaddfff504c
Landon <lyang006@ucr.edu>
2014-06-09 20:52:00 -0700

    gyu

--- Assets/Scripts/TowerStats.cs
+++ Assets/Scripts/TowerStats.cs
@@ -43,7 +43,7 @@ public class TowerStats : MonoBehaviour {
 		killStreakTimer = 20.0f;
 		theStreakTimer = killStreakTimer;
 		currCombo = comboKills;
-		theNextStreak = "Fire Rate Increase";
+		theNextStreak = "Fire Rate UP";
 		streakTimerIncr = 2.0f;
 		streakNoIncr = 2;
 		selectTower = 0;
@@ -185,7 +185,7 @@ public class TowerStats : MonoBehaviour {
 			{
 				f.GetComponent<upgradeCannons>().timeToUpgrade = true;
 			}
-			theNextStreak = "SPREADSHOT";
+			theNextStreak = "Triple Shot";
 			if((mFireRate - 0.1f) > 0.0f)
 				mFireRate -= 0.1f;
 			selectTower++;
@@ -198,7 +198,7 @@ public class TowerStats : MonoBehaviour {
 			{
 				f.GetComponent<upgradeCannons>().timeToUpgrade = true;
 			}
-			theNextStreak = "Fire Rate Increase";
+			theNextStreak = "Fire Rate UP";
 			bsm.numShots = 3;
 			//bsm.multiShot = true;
 			selectTower++;
@@ -212,7 +212,7 @@ public class TowerStats : MonoBehaviour {
 				f.GetComponent<upgradeCannons>().timeToUpgrade = true;
 			}
 			selectTower++;
-			theNextStreak = "MULTISHOT";
+			theNextStreak = "Burst Shot";
 			if((mFireRate - 0.1f) > 0.0f)
 				mFireRate -= 0.1f;
 			break;
@@ -228,7 +228,7 @@ public class TowerStats : MonoBehaviour {
 			selectTower++;
 			//GameObject go = GameObject.FindGameObjectWithTag("GameController");
 			//Buy_Shoot_Modes bsm = go.GetComponent<Buy_Shoot_Modes>();
-			theNextStreak = "Grenade";
+			theNextStreak = "Splash DMG";
 			bsm.numShots = 1;
 			//bsm.theTowerWeapon = 1;
 			bsm.multiShot = true;
@@ -241,7 +241,7 @@ public class TowerStats : MonoBehaviour {
 			selectTower++;
 			//GameObject go = GameObject.FindGameObjectWithTag("GameController");
 			//Buy_Shoot_Modes bsm = go.GetComponent<Buy_Shoot_Modes>();
-			theNextStreak = "Spread Grenade";
+			theNextStreak = "Triple Splash";
 			bsm.theTowerWeapon = 1;
 			bsm.multiShot = false;
 			break;
@@ -250,12 +250,12 @@ public class TowerStats : MonoBehaviour {
 			//towerType = (GameObject)Instantiate(towers[selectTower], new Vector3(-63.0f,36.0f,3.0f),
 			  //                                  Quaternion.identity);
 			selectTower++;
-			theNextStreak = "Radius Increase";
+			theNextStreak = "Radius UP";
 			//bsm.theRadiusMult += 1.0f;
 			bsm.numShots = 3;
 			break;
 		case 8:
-			theNextStreak = "Multi Grenade";
+			theNextStreak = "Burst Splash";
 			bsm.theRadiusMult += 1.0f;
 			break;
 		case 9:
